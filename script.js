82f337a28f730bebdce712a0cc025370232806c9
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-03 11:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ const form = document.getElementById('userForm');
+        
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            
+            const formData = new FormData(form);
+            let alertMessage = '';
+            
+            formData.forEach((value, key) => {
+                alertMessage += `${key}: ${value} `;
+            });
+            
+            alert(alertMessage.trim());
+        });
